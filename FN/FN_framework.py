bd03092978e516f2235bd57a8b47f48f560c8744
nagoyan777 <nagoyan777@hotmail.co.jp>
2020-02-19 23:39:16 +0900

    Add Observer and Logger base classes.

--- FN/FN_framework.py
+++ FN/FN_framework.py
@@ -1,4 +1,7 @@
-import os, io, re, numpy as np
+import os
+import io
+import re
+import numpy as np
 from collections import namedtuple, deque
 import tensorflow as tf
 from PIL import Image
@@ -8,6 +11,7 @@ import matplotlib.pyplot as plt
 
 Experience = namedtuple("Experience", ["s", "a", "r", "n_s", "d"])
 
+
 class FNAgent():
     def __init__(self, epsilon, actions):
         self.epsilon = epsilon
@@ -15,6 +19,7 @@ class FNAgent():
         self.model = None
         self.estimate_probs = False
         self.initialized = False
+
     def save(self, model_path):
         self.model.save(model_path, overwrite=True, include_optimizer=False)
 
@@ -54,7 +59,7 @@ class FNAgent():
             done = False
             episode_reward = 0
             while not done:
-                if render: 
+                if render:
                     env.render()
                 a = self.policy(s)
                 n_state, reward, done, info = env.step(a)
@@ -66,27 +71,37 @@ class FNAgent():
 
 
 class Trainer():
-    
-    def __init__(self, buffer_size=1024, batch_size=32, gamma=0.9, report_interval=10, log_dir=""):
+    def __init__(self,
+                 buffer_size=1024,
+                 batch_size=32,
+                 gamma=0.9,
+                 report_interval=10,
+                 log_dir=""):
         self.buffer_size = buffer_size
         self.batch_size = batch_size
         self.gamma = gamma
         self.report_interval = report_interval
         self.logger = Logger(log_dir, self.trainer_name)
         self.experiences = deque(maxlen=buffer_size)
-        self.training =False
+        self.training = False
         self.training_count = 0
         self.reward_log = []
 
     @property
     def trainer_name(self):
-        class_name = self. class . name
+        class_name = self.__class__.name
         snaked = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', class_name)
         snaked = re.sub('([a-z0-9])([A-Z])', r'\1_\2', snaked).lower()
         snaked = snaked.replace('_trainer', '')
         return snaked
 
-    def train_loop(self, env, agent, episode=200, initial_count=-1, render=False, observe_interval=0):
+    def train_loop(self,
+                   env,
+                   agent,
+                   episode=200,
+                   initial_count=-1,
+                   render=False,
+                   observe_interval=0):
         self.experiences = deque(maxlen=self.buffer_size)
         self.training = False
         self.training_count = 0
@@ -108,7 +123,8 @@ class Trainer():
                 n_state, reward, done, info = env.step(a)
                 e = Experience(s, a, reward, n_state, done)
                 self.experiences.append(e)
-                if not self.training and len(self.experiences) == self.buffer_size:
+                if not self.training and len(
+                        self.experiences) == self.buffer_size:
                     self.begin_train(i, agent)
                     self.trainig = True
 
@@ -122,7 +138,7 @@ class Trainer():
                 if not self.training and initial_count > 0 and i >= initial_count:
                     self.begin_train(i, agent)
                     self.training = True
-                
+
                 if self.training:
                     if len(frames) > 0:
                         self.logger.write_image(self.training_count, frames)
@@ -143,8 +159,135 @@ class Trainer():
         pass
 
     def is_event(self, count, interval):
-        return True if count != 0 and count % interval ==0 else False
+        return True if count != 0 and count % interval == 0 else False
 
     def get_recent(self, count):
         recent = range(len(self.experiences) - count, len(self.experiences))
         return [self.experiences[i] for i in recent]
+
+
+class Observer():
+    def __init__(self, env):
+        self._env = env
+
+    @property
+    def action_space(self):
+        return self._env.action_space
+
+    @property
+    def observation_space(self):
+        return self._env.observation_space
+
+    def reset(self):
+        return self.transform(self._env.reset())
+
+    def render(self):
+        self._env.render(mode='human')
+
+    def step(self, action):
+        n_state, reward, done, info = self._env.step(action)
+        return self.transform(n_state), reward, done, info
+
+    def transform(self, state):
+        raise NotImplementedError('You have to implement transform method')
+
+
+class Logger():
+    def __init__(self, log_dir="", dir_name=""):
+        self.log_dir = log_dir
+        if not log_dir:
+            self.log_dir = os.path.join(os.path.dirname(__file__), "logs")
+        if not os.path.exists(self.log_dir):
+            os.mkdir(self.log_dir)
+        if dir_name:
+            self.log_dir = os.path.join(self.log_dir, dir_name)
+            if not os.path.exists(self.log_dir):
+                os.mkdir(self.log_dir)
+
+        self._callback = tf.compat.v1.keras.callbacks.TensorBoard(self.log_dir)
+
+    @property
+    def writer(self):
+        return self._callback.writer
+
+    def set_model(self, model):
+        self._callback.set_model(model)
+
+    def path_of(self, file_name):
+        return os.path.join(self.log_dir, file_name)
+
+    def describe(self, name, values, episode=-1, step=-1):
+        mean = np.round(np.mean(values), 3)
+        std = np.round(np.std(values), 3)
+        desc = f"{name} is {mean} (+/-{std})"
+        if episode > 0:
+            print(f"At episode {episode}, {desc}")
+        elif step > 0:
+            print(f"At step {step}, {desc}")
+
+    def plot(self, name, values, interval=10):
+        indices = list(range(0, len(values), interval))
+        means = []
+        stds = []
+        for i in indices:
+            _values = values[i:(i + interval)]
+            means.append(np.mean(_values))
+            stds.append(np.std(_values))
+        means = np.array(means)
+        stds = np.array(stds)
+        plt.figure()
+        plt.title("{name} History")
+        plt.grid()
+        plt.fill_between(indices,
+                         means - stds,
+                         means + stds,
+                         alpha=0.1,
+                         color="g")
+        plt.plot(indices,
+                 means,
+                 f"o-",
+                 color="g",
+                 label="{name.lower()} per {interval} episode")
+        plt.legend(loc="best")
+        plt.show()
+
+    def write(self, index, name, value):
+        summary = tf.compat.v1.Summary()
+        summary_value = summary.value.add()
+        summary_value.tag = name
+        summary_value.simple_value = value
+        self.writer.add_summary(summary, index)
+        self.writer.flush()
+
+    def write_image(self, index, frames):
+        # Deal with a 'frames' as a list of sequential gray scaled image.
+        last_frames = [f[:, :, -1] for f in frames]
+        if np.min(last_frames[-1]) < 0:
+            scale = 127 / np.abs(last_frames[-1]).max()
+            offset = 128
+        else:
+            scale = 255 / np.max(last_frames[-1])
+            offset = 0
+        channel = 1  # gray scale
+        tag = f"frames_at_training_{index}"
+        values = []
+
+        for f in last_frames:
+            height, width = f.shape
+            array = np.asarray(f * scale + offset, dtype=np.uint8)
+            image = Image.fromarray(array)
+            output = io.BytesIO()
+            image.save(output, format="PNG")
+            image_string = output.getvalue()
+            output.close()
+            image = tf.compat.v1.Summary.Image(
+                height=height,
+                width=width,
+                colorspace=channel,
+                encoded_image_string=image_string)
+            value = tf.compat.v1.Summary.Value(tag=tag, image=image)
+            values.append(value)
+
+        summary = tf.compat.v1.Summary(value=values)
+        self.writer.add_summary(summary, index)
+        self.writer.flush()
